673ebbc237aeff48712104c0296fe56f9fd1a1f2
Paula Thomas <paula9t9@gmail.com>
2019-04-30 11:32:42 -0700

    search query added to location object

--- server.js
+++ server.js
@@ -13,21 +13,26 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('/public'));
 
 app.get('/location', (request, response) => {
-  response.send(searchLocation());
+  let locationObject = searchLocation(request.query.data);
+  console.log(locationObject);
+  response.send(locationObject);
+
 });
 
 
-app.use('*', (request, repsonse) => response.send('Sorry, that route does not exist'));
+app.use('*', (request, response) => response.send('Sorry, that route does not exist'));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
 
 
 //Function to search location
-function searchLocation(){
-  return new Location(require('./data/geo.json'));
+function searchLocation(searchQuery){
+  console.log(`Search query: ${searchQuery}`);
+  return new Location(searchQuery, require('./data/geo.json'));
 }
 
-function Location(data) {
+function Location(query, data) {
+  this.search_query = query;
   this.formatted_query= data.results[0].formatted_address;
   this.latitude = data.results[0].geometry.location.lat;
   this.longitude = data.results[0].geometry.location.lng;
